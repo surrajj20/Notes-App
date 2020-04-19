9b82443607db6791bca288eef0d5d36cc160f1f5
surrajj20 <shah.suraj1200@gmail.com>
2020-04-19 20:22:44 +0530

    chalk library install

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-const validator = require('validator')
+// const validator = require('validator')
+const chalk = require('chalk');
 const getNotes = require('./notes')
 
 const msg = getNotes();
 
 console.log(msg)
 
-console.log(validator.isURL('http://anugoonj-ipu.com'))
+console.log(chalk.green.inverse.bold('Sucess!'))
+
+// console.log(validator.isURL('http://anugoonj-ipu.com'))
 // const name = require('./utils');
 // console.log(name);
