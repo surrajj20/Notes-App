cba060dd4165b52a6428c3720f392efc32b86f58
surrajj20 <shah.suraj1200@gmail.com>
2020-04-19 19:04:08 +0530

    npm validator included

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+const validator = require('validator')
 const getNotes = require('./notes')
 
 const msg = getNotes();
 
 console.log(msg)
+
+console.log(validator.isURL('http://anugoonj-ipu.com'))
 // const name = require('./utils');
 // console.log(name);
