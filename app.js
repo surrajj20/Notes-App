6c661cfc3aec3adf399f98c2a884e5d32ec15d02
surrajj20 <shah.suraj1200@gmail.com>
2020-04-20 16:02:07 +0530

    Reading notes code is added

--- app.js
+++ app.js
@@ -57,8 +57,15 @@ yargs.command({
 yargs.command({
     command: 'read',
     describe: 'Read a note',
-    handler() {
-        console.log('Reading a notes')
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.readNote(argv.title)
     }
 })
 
--- notes.js
+++ notes.js
@@ -1,14 +1,11 @@
 const fs = require('fs')
 const chalk = require('chalk');
-const getNotes = () => {
-    return 'Your notes....'
-};
 
 const addNote = (title, body) => {
     const notes = loadNotes()
-    const duplicateNotes = notes.filter((note) => note.title === title)
+    const duplicateNote = notes.find((note) => note.title === title)
 
-    if (duplicateNotes.length === 0) {
+    if (!duplicateNote) {
         notes.push({
             title: title,
             body: body
@@ -43,6 +40,20 @@ const listNotes = () => {
 
 }
 
+const readNote = (title) => {
+    const notes = loadNotes()
+    const note = notes.find((note) => note.title === title)
+
+    if (note) {
+        console.log(chalk.green.inverse(note.title))
+        console.log(note.body)
+
+    } else {
+        console.log(chalk.red.inverse('Note not Found!'))
+    }
+
+}
+
 const saveNotes = (notes) => {
     const dataJSON = JSON.stringify(notes)
     fs.writeFileSync('notes.json', dataJSON)
@@ -62,9 +73,9 @@ const loadNotes = () => {
 }
 
 module.exports = {
-    getNotes: getNotes,
     addNote: addNote,
     removeNote: removeNote,
-    listNotes: listNotes
+    listNotes: listNotes,
+    readNote: readNote
 
 }
